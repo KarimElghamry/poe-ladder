e80f9cbfac1f4a6b35082362be54039c17269a34
KarimElghamry <Karimelghamry@gmail.com>
2020-02-01 20:26:36 +0200

    Updates

---
+++ precache-manifest.edf64661113ecb61091809ff983ad9df.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef322e4e1bacbc40918643f3c8dbba4",
+    "url": "/poe-ladder/index.html"
+  },
+  {
+    "revision": "2438d68bcf86e9066d77",
+    "url": "/poe-ladder/static/css/main.1ec81367.chunk.css"
+  },
+  {
+    "revision": "34d2ebf7535a5fa09710",
+    "url": "/poe-ladder/static/js/2.bcd49544.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/poe-ladder/static/js/2.bcd49544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2438d68bcf86e9066d77",
+    "url": "/poe-ladder/static/js/main.3a12b16c.chunk.js"
+  },
+  {
+    "revision": "5b6659f5e5ce96a2484e",
+    "url": "/poe-ladder/static/js/runtime-main.90cb16d4.js"
+  },
+  {
+    "revision": "640fefd2bf37910cc2b88eee12baca45",
+    "url": "/poe-ladder/static/media/Ascendant.640fefd2.png"
+  },
+  {
+    "revision": "645c162c477fcb257de476030289830d",
+    "url": "/poe-ladder/static/media/Assassin.645c162c.png"
+  },
+  {
+    "revision": "9d0eb44f5d8669c6ba58042832e9c3f2",
+    "url": "/poe-ladder/static/media/Berserker.9d0eb44f.png"
+  },
+  {
+    "revision": "635be8a74cedfa66d2692442d47cc2fd",
+    "url": "/poe-ladder/static/media/Champion.635be8a7.png"
+  },
+  {
+    "revision": "3e15859e0e98b9d353da81862ca10af1",
+    "url": "/poe-ladder/static/media/Chieftain.3e15859e.png"
+  },
+  {
+    "revision": "b465ea6c6279a7573b7c34d9fca3d4a4",
+    "url": "/poe-ladder/static/media/Deadeye.b465ea6c.png"
+  },
+  {
+    "revision": "ce85f13aa06ea3425d7db8cfeb57e347",
+    "url": "/poe-ladder/static/media/Duelist.ce85f13a.png"
+  },
+  {
+    "revision": "22c92a144b357279992692f5845250ec",
+    "url": "/poe-ladder/static/media/Elementalist.22c92a14.png"
+  },
+  {
+    "revision": "3a5472bdba730aa8fc43ce6aa6dcf723",
+    "url": "/poe-ladder/static/media/Fontin.3a5472bd.ttf"
+  },
+  {
+    "revision": "06b0dba78c15396d8df7596b1e1fcd20",
+    "url": "/poe-ladder/static/media/Gladiator.06b0dba7.png"
+  },
+  {
+    "revision": "185cd3b6093a60b5b0ad3f73badc28c1",
+    "url": "/poe-ladder/static/media/Guardian.185cd3b6.png"
+  },
+  {
+    "revision": "ef9c7279c057b9770c7d3ee30f84dbcf",
+    "url": "/poe-ladder/static/media/Hierophant.ef9c7279.png"
+  },
+  {
+    "revision": "ef10570d4155e9f1f6229a2b9d87daef",
+    "url": "/poe-ladder/static/media/Inquisitor.ef10570d.png"
+  },
+  {
+    "revision": "7399cf5616d2816c5585efa5b9766e37",
+    "url": "/poe-ladder/static/media/Juggernaut.7399cf56.png"
+  },
+  {
+    "revision": "68ca165a2e820d4676f9bf8bc6b92aac",
+    "url": "/poe-ladder/static/media/Marauder.68ca165a.png"
+  },
+  {
+    "revision": "71fdd0090aa47f866a33ce6703c0330a",
+    "url": "/poe-ladder/static/media/Necromancer.71fdd009.png"
+  },
+  {
+    "revision": "0ef5775fc3ef079ff8c46382342cec29",
+    "url": "/poe-ladder/static/media/Occultist.0ef5775f.png"
+  },
+  {
+    "revision": "c666733f2666eb5734f094e801d14dd7",
+    "url": "/poe-ladder/static/media/Pathfinder.c666733f.png"
+  },
+  {
+    "revision": "ef62b3fc8618edccd9e5221645d891c3",
+    "url": "/poe-ladder/static/media/Raider.ef62b3fc.png"
+  },
+  {
+    "revision": "60d6ff98a84f2dce4aae8cf61624d681",
+    "url": "/poe-ladder/static/media/Ranger.60d6ff98.png"
+  },
+  {
+    "revision": "7b0f2f3895ecaec557fd6d832d470db7",
+    "url": "/poe-ladder/static/media/Saboteur.7b0f2f38.png"
+  },
+  {
+    "revision": "640fefd2bf37910cc2b88eee12baca45",
+    "url": "/poe-ladder/static/media/Scion.640fefd2.png"
+  },
+  {
+    "revision": "3d1441c527424de300901c8b9607a9e2",
+    "url": "/poe-ladder/static/media/Shadow.3d1441c5.png"
+  },
+  {
+    "revision": "22be65897f4433a7c6a897b5901bb9b3",
+    "url": "/poe-ladder/static/media/Slayer.22be6589.png"
+  },
+  {
+    "revision": "b6f94e26b58c7d069978db19a671d0c4",
+    "url": "/poe-ladder/static/media/Templar.b6f94e26.png"
+  },
+  {
+    "revision": "bd0482289b60c3bf81ca034fd17d50d4",
+    "url": "/poe-ladder/static/media/Trickster.bd048228.png"
+  },
+  {
+    "revision": "644d9fad8dde4588ec6252d34e88553f",
+    "url": "/poe-ladder/static/media/Witch.644d9fad.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Poe_Ladder/precache-manifest.cfe1d718322e58e92dd06edaef8fb526.js"
+  "/poe-ladder/precache-manifest.edf64661113ecb61091809ff983ad9df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Poe_Ladder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poe-ladder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
